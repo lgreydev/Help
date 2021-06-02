a7ff545bdd95bf56e01763f0f720fcaa8211e968
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-02 10:56:41 +0300

    Test 3

--- Help/ViewController.swift
+++ Help/ViewController.swift
@@ -13,10 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-   
-    class User {
-        
-    }
 
 }
 
