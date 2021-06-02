18d8efbd09781447de483d51b6b231333a12f66d
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-02 10:14:35 +0300

    Add example of a clean class

---
+++ Help/CleanClass.swift
@@ -0,0 +1,47 @@
+//
+//  PureClass.swift
+//  Help
+//
+//  Created by Sergey Lukaschuk on 02.06.2021.
+//
+
+import Foundation
+
+/// Example of a pure class
+/// Пример чистого класса
+
+/*
+final class CleanViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // MARK: - Public Properties
+    var userID: String?
+    weak var delegate: SomeDelegate?
+    
+    // MARK: - Private Properties
+    private let userService = UserService()
+    private var userList: [User]?
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationBar()
+    }
+    
+    // MARK: - Private Methods
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = .red
+        navigationItem.title = "Some"
+    }
+    
+    // MARK: - IBActions
+    @IBAction private func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+*/
